4a42f369095a9f9e01c4e707cc02e44aee092e44
mrbt <k@gmail.comg>
2021-11-30 11:49:35 +0530

    fix test 2

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@ echo "Cloning Repo...."
 cd /home/absentsun/
 git clone https://github.com/WeakestHunter43/TestVC VCPlayerBot
 cd VCPlayerBot
+pip3 install
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
