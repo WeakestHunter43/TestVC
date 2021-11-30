2e832541d669ee0541c9a30a768ca6cb4b14a70e
mrbt <k@gmail.comg>
2021-11-30 14:09:01 +0530

    pip3

--- bot.sh
+++ bot.sh
@@ -1,5 +1,5 @@
 ls -a /usr/local/lib/python3.10/site-packages/
-rm -rfv /usr/local/lib/python3.10/site-packages/.wh-pip
+rm -rfv /usr/local/lib/python3.10/site-packages/.wh-pip*
 pip3 install -U pip --no-cache-dir
 pip3 install -U -r requirements.txt --no-cache-dir
 ls -a /usr/local/lib/python3.10/site-packages/
