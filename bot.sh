02a8d60c6b56a4eb40977d7d7fc41adc47eefd40
mrbt <k@gmail.comg>
2021-11-30 13:07:59 +0530

    nodejs17

--- bot.sh
+++ bot.sh
@@ -1,4 +1,5 @@
 echo $(rm -rfv /usr/local/lib/python3.9/site-packages/~*)
+echo $(rm -rfv /usr/local/lib/python3.10/site-packages/~*)
 pip3 install -U pip
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
