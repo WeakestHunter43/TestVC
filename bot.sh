d967a161b1ff2658ba48de8f3027acd7830a5906
mrbt <k@gmail.comg>
2021-11-30 13:45:26 +0530

    pip3

--- bot.sh
+++ bot.sh
@@ -1,10 +1,3 @@
-echo $(rm -rfv /usr/local/lib/python3.9/site-packages/~*)
-echo $(rm -rfv /usr/local/lib/python3.10/site-packages/~*)
-#Write all modules to a txt file
-pip freeze > F.txt
-#Now to remove one by one:
-pip uninstall -r F.txt -y
-pip install -r F.txt --no-cache-dir
 pip3 install -U pip --no-cache-dir
 pip3 install -U -r requirements.txt --no-cache-dir
 echo "Starting Bot...."
