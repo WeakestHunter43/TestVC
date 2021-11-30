8a40cd2e32c7e406415c9c3352c3d33c916d6a8d
mrbt <k@gmail.comg>
2021-11-30 12:53:34 +0530

    test xxx

---
+++ bot.sh
@@ -0,0 +1,5 @@
+rm -rfv /usr/local/lib/python3.9/site-packages/~*
+pip3 install -U pip
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 main.py
--- start.sh
+++ start.sh
@@ -1,10 +1,6 @@
 echo "Cloning Repo...."
-cd /usr/local/lib/python3.9/site-packages
-echo $(rm -rfv ~*)
 cd /home/absentsun/
 git clone https://github.com/WeakestHunter43/TestVC VCPlayerBot
 cd VCPlayerBot
-pip3 install -U pip
-pip3 install -U -r requirements.txt
-echo "Starting Bot...."
-python3 main.py
+chmod +x bot.sh
+./bot.sh
