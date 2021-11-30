6e5d3399509833ba32c45bfe218f179bf1fb2b96
mrbt <k@gmail.comg>
2021-11-30 13:54:05 +0530

    pip3

--- bot.sh
+++ bot.sh
@@ -1,4 +1,4 @@
-rm -rfv /usr/local/lib/python3.10/site-packages/wh-pip
+ls /usr/local/lib/python3.10/site-packages/
 pip3 install -U pip --no-cache-dir
 pip3 install -U -r requirements.txt --no-cache-dir
 echo "Starting Bot...."
