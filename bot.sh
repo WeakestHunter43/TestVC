a39e79ca68e84f54cbdaddf9e885f8ea18c1b5df
mrbt <k@gmail.comg>
2021-11-30 13:33:03 +0530

    pip3

--- bot.sh
+++ bot.sh
@@ -1,7 +1,7 @@
 echo $(rm -rfv /usr/local/lib/python3.9/site-packages/~*)
 echo $(rm -rfv /usr/local/lib/python3.10/site-packages/~*)
-python3 -m pip install --upgrade pip
-pip3 freeze > unins && pip3 uninstall -y -r unins && pip3 install -y -r unins
+pip3 install --upgrade pip
+pip3 freeze > unins && pip3 uninstall -y -r unins && pip3 install -U -r unins
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
