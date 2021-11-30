e25b043bea1a9374846998ce99f44cef62ca45bb
mrbt <k@gmail.comg>
2021-11-30 12:12:40 +0530

    fix test

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 echo "Cloning Repo...."
 cd /home/absentsun/
+ls /usr/local/lib/python3.9/site-packages/
 rm -rfv /usr/local/lib/python3.9/site-packages/~*
 git clone https://github.com/WeakestHunter43/TestVC VCPlayerBot
 cd VCPlayerBot
